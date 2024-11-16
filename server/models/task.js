ea1ac998220941d3fdef455bb807a194be7e28be
instinct28 <yk267164@gmail.com>
2024-11-16 12:11:15 +0530

    Project Setup

---
+++ server/models/task.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    task_description: { 
+        type: String, 
+        required: true 
+    },
+    task_type: { 
+        type: String, 
+        required: true, 
+        default: "work" 
+    },
+    start_time: { 
+        type: Date, 
+        required: true, 
+        default: Date.now() 
+    },
+    time_taken: { 
+        type: Number, 
+        required: true, 
+        default: 0 
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Tasks", taskSchema);
